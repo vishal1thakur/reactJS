9b3a74ea5c8c86d60fcf63e5a3b601820a84c709
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-22 13:43:37 +0530

    Displaying user multiple user cards

---
+++ github-finder/src/components/users/Useritem.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Useritem extends Component {
+  render() {
+    const {login, avatar_url, html_url} = this.props.user;
+    return (
+      <div className="card text-center">
+        <img
+          src={avatar_url}
+          className="round-img"
+          alt=""
+          style={{width: '60px'}}
+        />
+        <h3>{login}</h3>
+
+        <div>
+          <a href={html_url} className="btn btn-dark btn-sm my-1">
+            More
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Useritem;
