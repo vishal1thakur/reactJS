ebad9972e6dbefefa74c6871e4f22e8277a5fca5
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-22 14:49:48 +0530

    Fetching data from GitHub API and displaying it on the ui

--- github-finder/src/components/users/Users.js
+++ github-finder/src/components/users/Users.js
@@ -3,32 +3,10 @@ import Useritem from './Useritem';
 import PropTypes from 'prop-types';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 1,
-        login: 'mojombo',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        id: 2,
-        login: 'defunkt',
-        avatar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt',
-      },
-      {
-        id: 3,
-        login: 'pjhyett',
-        avatar_url: 'https://avatars.githubusercontent.com/u/3?v=4',
-        html_url: 'https://github.com/pjhyett',
-      },
-    ],
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map((user) => (
+        {this.props.users.map((user) => (
           <Useritem key={user.id} user={user} />
         ))}
       </div>
@@ -42,7 +20,7 @@ const userStyle = {
   gridGap: '1rem',
 };
 
-Useritem.PropTypes = {
+Useritem.prototype = {
   user: PropTypes.object.isRequired,
 };
 
