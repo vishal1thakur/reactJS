7bda23677465f2ac49ada0df150c65bbe2c31e6a
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-24 17:33:29 +0530

    Alert for empty search

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // Fetch default users for testing
@@ -30,7 +32,14 @@ class App extends Component {
     this.setState({users: res.data.items, loading: false});
   };
 
+  // Clear users from state
   clearUsers = () => this.setState({users: [], loading: false});
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({alert: {msg, type}});
+    setTimeout(() => this.setState({alert: null}), 5000);
+  };
 
   render() {
     const {users, loading} = this.state;
@@ -38,10 +47,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
