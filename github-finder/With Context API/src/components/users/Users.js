2b9aea220cb1dd34ee7176dc20f90cf5f2d19fa3
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-25 16:27:32 +0530

    File structure for refactoring with context API

---
+++ github-finder/With Context API/src/components/users/Users.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Useritem from './Useritem';
+import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
+
+const Users = ({users, loading}) => {
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div style={userStyle}>
+        {users.map((user) => (
+          <Useritem key={user.id} user={user} />
+        ))}
+      </div>
+    );
+  }
+};
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem',
+};
+
+Useritem.prototype = {
+  user: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default Users;
