933060c9fc65af52626f3f7e89cd204b0ff4a6b2
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-27 19:54:57 +0530

    Brought in songs and song to App & created styles for a song

--- hymn/src/App.js
+++ hymn/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 // Import Styles
 import './styles/app.scss';
 // Adding components
 import Player from './components/Player';
 import Song from './components/Song';
+// Import Util
+import data from './util';
 
 function App() {
+  // State
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
   return (
     <div className="App">
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
