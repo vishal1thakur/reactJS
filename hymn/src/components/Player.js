f06739ff81a8608f670ae0ab511df40c1eb256a4
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-28 02:37:07 +0530

    Skip forward backward functionality

--- hymn/src/components/Player.js
+++ hymn/src/components/Player.js
@@ -16,6 +16,8 @@ const Player = ({
   setIsPlaying,
   setSongInfo,
   songInfo,
+  songs,
+  setCurrentSong,
 }) => {
   // 2) Event Handlers
   // 2.1) Play + Pause funtionality
@@ -36,6 +38,21 @@ const Player = ({
   const dragHandler = (e) => {
     audioRef.current.currentTime = e.target.value;
     setSongInfo({...songInfo, current: e.target.value});
+  };
+
+  // 2.4) Skipping Forward & Backward
+  const skipTrackHandler = (direction) => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    if (direction === 'skip-forward') {
+      setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+    }
+    if (direction === 'skip-back') {
+      if ((currentIndex - 1) % songs.length === -1) {
+        setCurrentSong(songs[songs.length - 1]);
+      } else {
+        setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      }
+    }
   };
 
   // 3) Functions
@@ -64,7 +81,8 @@ const Player = ({
       </div>
       <div className="play-control">
         <FontAwesomeIcon
-          className="skip-forward"
+          onClick={() => skipTrackHandler('skip-back')}
+          className="skip-back"
           size="2x"
           icon={faAngleLeft}
         />
@@ -74,7 +92,12 @@ const Player = ({
           size="2x"
           icon={isPlaying ? faPause : faPlay}
         />
-        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleRight} />
+        <FontAwesomeIcon
+          onClick={() => skipTrackHandler('skip-forward')}
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
       </div>
     </div>
   );
