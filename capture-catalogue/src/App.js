1e9d9ebeb02c9b7d5890d6cab0d67c48b144f8c5
Vishal Thakur <interaction.vishal@gmail.com>
2021-03-30 20:28:05 +0530

    Styled component for About Us section

--- capture-catalogue/src/App.js
+++ capture-catalogue/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// Global Style
+import GlobalStyle from './components/GlobalStyle';
 import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
