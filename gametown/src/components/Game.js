64324f6a132b1f2918003e59a544266a95af25ea
Vishal Thakur <interaction.vishal@gmail.com>
2021-04-13 20:47:26 +0530

    Transition animations

--- gametown/src/components/Game.js
+++ gametown/src/components/Game.js
@@ -6,6 +6,7 @@ import {useDispatch} from 'react-redux';
 import {loadDetail} from '../actions/detailAction';
 import {Link} from 'react-router-dom';
 import {smallImage} from '../Util';
+import {popup} from '../animations';
 
 const Game = ({name, released, image, id}) => {
   const stringPathId = id.toString();
@@ -17,7 +18,13 @@ const Game = ({name, released, image, id}) => {
   };
 
   return (
-    <StyledGame layoutId={stringPathId} onClick={loadDetailHandler}>
+    <StyledGame
+      variants={popup}
+      initial="hidden"
+      animate="show"
+      layoutId={stringPathId}
+      onClick={loadDetailHandler}
+    >
       <Link to={`/game/${id}`}>
         <motion.h3 layoutId={`title ${stringPathId}`}>{name}</motion.h3>
         <p>{released}</p>
--- gametown/src/pages/Home.js
+++ gametown/src/pages/Home.js
@@ -9,6 +9,7 @@ import GameDetail from '../components/GameDetail';
 import styled from 'styled-components';
 import {motion, AnimatePresence, AnimateSharedLayout} from 'framer-motion';
 import {useLocation} from 'react-router-dom';
+import {fadeIn} from '../animations';
 
 const Home = () => {
   // Get the current location
@@ -25,7 +26,7 @@ const Home = () => {
   );
 
   return (
-    <GameList>
+    <GameList variants={fadeIn} inital="hidden" animate="show">
       <AnimateSharedLayout type="crossfade">
         <AnimatePresence>
           {pathId && <GameDetail pathId={pathId} />}
