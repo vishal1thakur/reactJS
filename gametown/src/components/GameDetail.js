77ad1c62120afa6f8d0d758f9af1d3358ecb4d6e
Vishal Thakur <interaction.vishal@gmail.com>
2021-04-13 17:46:48 +0530

    Icons for platforms

--- gametown/src/components/GameDetail.js
+++ gametown/src/components/GameDetail.js
@@ -5,6 +5,13 @@ import {motion} from 'framer-motion';
 import {useSelector} from 'react-redux';
 import {useHistory} from 'react-router-dom';
 import {smallImage} from '../Util';
+//Images
+import playstation from '../img/playstation.svg';
+import steam from '../img/steam.svg';
+import xbox from '../img/xbox.svg';
+import nintendo from '../img/nintendo.svg';
+import apple from '../img/apple.svg';
+import gamepad from '../img/gamepad.svg';
 
 const GameDetail = ({pathId}) => {
   const history = useHistory();
@@ -16,6 +23,24 @@ const GameDetail = ({pathId}) => {
       history.push('/');
     }
   };
+  // Get platform images
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case 'Playstation 4':
+        return playstation;
+      case 'Xbox One':
+        return xbox;
+      case 'PC':
+        return steam;
+      case 'Nintendo Switch':
+        return nintendo;
+      case 'iOS':
+        return apple;
+      default:
+        return gamepad;
+    }
+  };
+
   // Data
   const {game, screen, isLoading} = useSelector((state) => state.details);
   return (
@@ -32,7 +57,11 @@ const GameDetail = ({pathId}) => {
                 <h3>Platforms</h3>
                 <Platforms className="platforms">
                   {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img
+                      alt={data.platform.name}
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                    />
                   ))}
                 </Platforms>
               </Info>
@@ -45,7 +74,7 @@ const GameDetail = ({pathId}) => {
               />
             </Media>
             <Description>
-              <p>{game.description_raw}</p>
+              <p>{game.description_raw || 'No Description Available'}</p>
             </Description>
             <div className="gallery">
               {screen.results.map((screen) => (
